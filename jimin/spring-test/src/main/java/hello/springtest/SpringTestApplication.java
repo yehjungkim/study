08b721288b30a309c7372de41eef06cb1b053a56
Jimin <thswlals0219@gmail.com>
2023-08-20 15:32:11 +0900

    스프링 시큐리티 설정

--- jimin/spring-test/src/main/java/hello/springtest/SpringTestApplication.java
+++ jimin/spring-test/src/main/java/hello/springtest/SpringTestApplication.java
@@ -5,14 +5,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
 @SpringBootApplication
 public class SpringTestApplication {
-
-	@RequestMapping("/")
-	String home() {
-		return "Hello World!";
-	}
 
 	public static void main(String[] args) {
 

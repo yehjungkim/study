22f758b53fa23ac95848131b71574958014cc5fc
EraseDa <fkakrla123@gmail.com>
2023-08-28 14:13:52 +0900

    로그인 및 회원가입 서비스 구현

--- jiwoo/src/main/java/Member/MemberService.java
+++ jiwoo/src/main/java/Member/MemberService.java
@@ -1,8 +1,11 @@
 package Member;
 
+import org.springframework.stereotype.Service;
+
 public interface MemberService {
 
     //회원 가입
     void join(Member member);
 
+    Member login(String id, String pw);
 }

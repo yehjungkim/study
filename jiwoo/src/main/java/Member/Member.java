0fa964a7cde45a43a10f54c9e65892c286136f64
EraseDa <fkakrla123@gmail.com>
2023-08-28 13:24:32 +0900

    Member 엔티티 추가

--- jiwoo/src/main/java/Member/Member.java
+++ jiwoo/src/main/java/Member/Member.java
@@ -1,81 +1,71 @@
 package Member;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.ColumnDefault;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+import javax.persistence.UniqueConstraint;
 import java.util.Date;
-
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name="MEMBER", uniqueConstraints = {@UniqueConstraint(
+        name = "ID_EMAIL_UNIQUE",
+        columnNames = {"ID","EMAIL"}
+)})
 public class Member {
+    @Id
+    @GeneratedValue(strategy= GenerationType.IDENTITY)
+    @Column(name="MEMBER_PK")
+    private long member_pk;
 
-    private int member_pk;
-    private int auth_cd_pk;
+    @Enumerated(EnumType.STRING)
+    private Authority authority;
+
+    @Column(name="NICKNAME", nullable = false, length = 10)
     private String nickname;
+
+
+    @Column(name = "ID", nullable = false, length = 10)
     private String id;
+
+
+    @Column(name = "PW", nullable = false, length = 15)
     private String pw;
+
+    @Column(name = "EMAIL", nullable = false, length = 30)
     private String email;
-    private Date dormant_account_YMD;
 
-    public Member(int member_pk, int auth_cd_pk, String nickname, String id, String pw, String email, Date dormant_account_YMD) {
-        this.member_pk = member_pk;
-        this.auth_cd_pk = auth_cd_pk;
+    @Temporal(TemporalType.TIMESTAMP)
+     private Date dormant_account_YMD;
+
+    public Member(String nickname, String id, String pw, String email) {
         this.nickname = nickname;
         this.id = id;
         this.pw = pw;
         this.email = email;
-        this.dormant_account_YMD = dormant_account_YMD;
     }
 
-    public int getMember_pk() {
-        return member_pk;
-    }
-
-    public void setMember_pk(int member_pk) {
-        this.member_pk = member_pk;
-    }
-
-    public int getAuth_cd_pk() {
-        return auth_cd_pk;
-    }
-
-    public void setAuth_cd_pk(int auth_cd_pk) {
-        this.auth_cd_pk = auth_cd_pk;
-    }
-
-    public String getNickname() {
-        return nickname;
-    }
-
-    public void setNickname(String nickname) {
-        this.nickname = nickname;
-    }
-
-    public String getId() {
-        return id;
-    }
-
-    public void setId(String id) {
-        this.id = id;
-    }
-
-    public String getPw() {
-        return pw;
-    }
-
-    public void setPw(String pw) {
-        this.pw = pw;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public Date getDormant_account_YMD() {
-        return dormant_account_YMD;
-    }
-
-    public void setDormant_account_YMD(Date dormant_account_YMD) {
-        this.dormant_account_YMD = dormant_account_YMD;
+    public Member createMember(SignUpRequestDto signUpDto) {
+        Member member = new Member();
+        member.setAuthority(Authority.MEMBER);
+        member.setNickname(signUpDto.getNickname());
+        member.setId(signUpDto.getId());
+        member.setPw(signUpDto.getPw());
+        member.setEmail(signUpDto.getEmail());
+        return member;
     }
 
 }

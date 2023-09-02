09968f60586e163ff807f9bde375ab9b084d704d
EraseDa <fkakrla123@gmail.com>
2023-09-03 00:22:31 +0900

    build : 그레이들 jwt 추가

---
+++ jiwoo/src/main/java/common/ApiErrorEnum.java
@@ -0,0 +1,31 @@
+package Common;
+
+import lombok.Getter;
+import lombok.ToString;
+import org.springframework.http.HttpStatus;
+
+@Getter
+@ToString
+public enum ErrorEnum {
+    // 클라이언트 에러
+    BAD_REQUEST(HttpStatus.BAD_REQUEST, "CE400"),
+    Forbidden(HttpStatus.FORBIDDEN, "CE403"),
+    NOT_FOUND(HttpStatus.NOT_FOUND, "CE404"),
+    // 커스텀 에러
+    SECURITY_01(HttpStatus.UNAUTHORIZED, "S0001", "로그인이 필요합니다");
+
+    private final HttpStatus status;
+    private String message;
+    private final String code;
+
+    ErrorEnum(HttpStatus status, String code) {
+        this.status = status;
+        this.code = code;
+    }
+
+    ErrorEnum(HttpStatus status, String code, String message) {
+        this.status = status;
+        this.code = code;
+        this.message = message;
+    }
+}

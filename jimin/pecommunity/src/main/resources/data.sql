de205f7a325d0785de2346443ea498244b6effdb
Jimin <thswlals0219@gmail.com>
2023-09-09 17:47:34 +0900

    feat: 서버 실행 시, 초기 데이터 자동 삽입

---
+++ jimin/pecommunity/src/main/resources/data.sql
@@ -0,0 +1,4 @@
+INSERT INTO board (board_cd_pk, board_nm, sports) VALUES (1, '축구 게시판', '축구');
+INSERT INTO board (board_cd_pk, board_nm, sports) VALUES (2, '배구 게시판', '배구');
+INSERT INTO board (board_cd_pk, board_nm, sports) VALUES (3, '농구 게시판', '농구');
+INSERT INTO board (board_cd_pk, board_nm, sports) VALUES (4, '배드민턴 게시판', '배드민턴');

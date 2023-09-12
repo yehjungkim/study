396da481663be89dc1a008bc825b68f9efec95a7
EraseDa <fkakrla123@gmail.com>
2023-09-12 17:59:54 +0900

    feat: Post 엔티티 생성

---
+++ jiwoo/src/main/java/post/Post.java
@@ -0,0 +1,50 @@
+package post;
+
+import board.Board;
+import lombok.Getter;
+import member.Member;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+import java.util.Date;
+
+@Entity
+@Table(name="POST")
+@Getter
+public class Post {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name="POST_PK")
+    private int post_pk;
+
+    @ManyToOne
+    @JoinColumn(name="SPORTS_CD_FK")
+    private Board sports_cd_fk;
+
+    @ManyToOne
+    @JoinColumn(name="MEMBER_FK")
+    private Member member_fk;
+
+    @Column(name = "TITLE", nullable = false, length = 50)
+    private String title;
+
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date create_ymd;
+
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date update_ymd;
+
+    @Column(name = "CONTENT", nullable = false)
+    private String content;
+
+    @Column(name = "VIEW_CNT", nullable = false)
+    private int view_cnt;
+}

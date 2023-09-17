d05bd54f51637ba76380bce0e2536d94a0a17530
yehjungkim <109449417+yehjungkim@users.noreply.github.com>
2023-09-17 17:06:15 +0900

    fix: 미반영 코드 수정

--- study/springsecurity/src/main/java/com/example/auth/PrincipalDetails.java
+++ study/springsecurity/src/main/java/com/example/auth/PrincipalDetails.java
@@ -6,19 +6,36 @@ package com.example.auth;
 //User 오브젝트 타입 => userDetail 타입 객체
 
 import com.example.model.User;
+import lombok.Data;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.oauth2.core.user.OAuth2User;
 
 import java.util.ArrayList;
 import java.util.Collection;
+import java.util.Map;
 
-//security session 에 session 정보를 저장하는데, 거기에 들어갈 수 있는 객체가 authentication
-public class PrincipalDetails implements UserDetails {
+/*security session 에 session 정보를 저장하는데, 거기에 들어갈 수 있는 객체가 authentication
+/*
+* */
+@Data
+public class PrincipalDetails implements UserDetails, OAuth2User {
 
     private User user;
+    private Map<String,Object> attributes;
 
     public PrincipalDetails(User user){
         this.user =user;
+    }
+
+    public PrincipalDetails(User user, Map<String,Object> attributes){
+        this.user =user;
+        this.attributes = attributes;
+    }
+
+    @Override
+    public Map<String, Object> getAttributes() {
+        return attributes;
     }
 
     //해당 유저의 권한을 return하는 곳
@@ -64,5 +81,10 @@ public class PrincipalDetails implements UserDetails {
 
         //우리 사이트에서 1년동안 회원이 로그인을 안하면 휴면 계정으로 하기로 함
         return true;
+    }
+
+    @Override
+    public String getName() {
+        return null;
     }
 }
